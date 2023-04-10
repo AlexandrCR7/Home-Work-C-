486bb281d3c75693dae90fa64ef1c34ba7081561
Alexandr <k9108@mail.ru>
2023-04-10 15:47:43 +0300

    63 строка, вместо < добавил <=

--- Program.cs
+++ Program.cs
@@ -60,7 +60,7 @@
 System.Console.WriteLine("Введите число: ");
 int N = int.Parse(Console.ReadLine());
 int i = 1;
-while(i < N){
+while(i <= N){
      if(i % 2 == 0)
      System.Console.Write(i + ", ");
      i++;
